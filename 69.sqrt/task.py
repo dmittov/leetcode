b9fa8bb0c4326b6c8776c2c627cf0f89ca95f65a
Dmitry Mittov <mittov@gmail.com>
2021-01-31 11:00:33 +0100

    [task #69] int division

--- 69.sqrt/task.py
+++ 69.sqrt/task.py
@@ -2,7 +2,7 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         lo, hi = 0, x + 2
         while (lo + 1) < hi:
-            median = int((lo + hi) / 2)
+            median = (lo + hi) // 2
             median_sq = median ** 2
             if median_sq > x:
                 hi = median
